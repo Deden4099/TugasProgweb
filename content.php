5b43e2d78c4b257c14889b370c6627d4d3a1d944
Deden <dedennursyadid@gmail.com>
2017-04-28 18:21:56 +0700

    Update content

--- content.php
+++ content.php
@@ -3,12 +3,14 @@ if(isset($_GET['page'])){
 	$page=$_GET['page'];	
 	$file="$page.php";
 	
+	//untuk mengkoneksi ke dasboard
 	if (!file_exists($file)){
 		include ("dashboard.php");
 	}else{
 		include ("$page.php");
 	}
 }else{
+	//untuk mengkoneksi ke dasboard
 	include ("dashboard.php");
 }
 ?>
